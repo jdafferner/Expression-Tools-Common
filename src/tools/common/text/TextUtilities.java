24f21f164411f614f2690c418211d16742766d08
jdafferner <james.dafferner@gmail.com>
2017-10-06 13:08:22 -0500

    Commit and Push 10/6

--- src/tools/common/text/TextUtilities.java
+++ src/tools/common/text/TextUtilities.java
@@ -1,49 +1,105 @@
 package tools.common.text;
 
-import java.awt.List;
+
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.Type;
-import java.lang.reflect.TypeVariable;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
+import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.stream.Stream;
+
+import org.apache.commons.io.IOUtils;
 
 public class TextUtilities {
 	
 	private ArrayList<?> text;
-	private Stream<?> in;
+	private int span;
+	private byte[] byteArray;
 	
 	public TextUtilities() {
-		text = new ArrayList();
+		text = new ArrayList<String>();
+		setSpan(75);
 	}
-	public TextUtilities(File file) {
-		try {
-			setStream(Files.lines(file.toPath()));
-			text = new ArrayList<String>();
-			getStream().forEach((String) ->{
-				
-			});
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	private void setSpan(int s) {
+		span = s;
+	}
+	private int getSpan() {
+		return span;
+	}
+	private void setByteArray(byte[] bytes) {
+		
+	}
+	private byte[] getByteArray() {
+		return byteArray;
+	}
+	private void entryFile(File file) {
+		Path path;
+		try{
+			path = file.toPath();
+			
+		}catch(InvalidPathException e) {
+			System.out.println("Cannot resolve file path.");
 		}
 	}
-	public void bulletList() {
-	 
+	private void entryText() {
+		Scanner scan = new Scanner(System.in);
+		byte [] bytes = new byte[4000];
+		int z = 0;
+		System.out.println("Input text once finished press F5 to exit.");
+		while(scan.hasNextByte()) {
+			z++;
+			if(z > bytes.length) {
+				int d = (bytes.length)*2;
+				byte [] temp = new byte[d];
+				for(int i = 0; i < bytes.length; i++ ) {
+					temp[i] = bytes[i];
+				}
+				bytes = new byte[d];
+				bytes = temp;
+				temp = null;
+			}
+			byte b = scan.nextByte();
+			if(b == 95) {
+				break;
+			}
+			else {
+				bytes[z] = b;
+			}
+		}
+		scan.close();
 	}
-	private void setStream(Stream<?> s) {
-		in = s;
+	private void writeArrayList(byte[] bytes) {
+		text = new ArrayList<String>();
+		int r = bytes.length;
+		
 	}
-	private Stream<?> getStream() {
-		return in;
-	}
+
 	public ArrayList<?> getTextArrayList(){										//text ArrayList pointer
 		return text;
 	}
-
-	
+	public void doCenterText() {
+		
+	}
+	public void doLeftJustifyText() {
+		 
+	}
+	public void doRightJustifyText() {
+		
+	}
+//	if(input.nextByte() == 14) {
+//		System.out.println("Back Space");
+//		if(bytes.length > 0) z--;
+//		else z = 0;
+//		
+//		bytes[z] = null;
+//	}
+//	if(input.hasNextByte()) {
+//		z++;
+//		bytes[z] = input.nextByte();
+//	}
 }
